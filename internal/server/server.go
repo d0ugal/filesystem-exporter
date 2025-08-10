6aafe69c3e3172e19206f0318eeeb6b4f8656332
Dougal Matthews <dougal@dougalmatthews.com>
2025-08-10 19:14:07 +0000

    fix: update golangci-lint configuration and server improvements

--- internal/server/server.go
+++ internal/server/server.go
@@ -9,6 +9,7 @@ import (
 
 	"filesystem-exporter/internal/config"
 	"filesystem-exporter/internal/metrics"
+
 	"github.com/gin-gonic/gin"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
